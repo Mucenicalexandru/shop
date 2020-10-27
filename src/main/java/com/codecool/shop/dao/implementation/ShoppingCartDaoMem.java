2515a7ada11d953a8a31b5232583aab5ac3f6892
Mucenicalexandru <mucenic.alexandru@gmail.com>
2020-10-27 09:27:12 +0200

    Cart dao built

---
+++ src/main/java/com/codecool/shop/dao/implementation/ShoppingCartDaoMem.java
@@ -0,0 +1,44 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.CartDao;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCartDaoMem implements CartDao {
+    private List<Product> shoppingCart = new ArrayList<>();
+    private static ShoppingCartDaoMem instance = null;
+
+    @Override
+    public void add(Product product) {
+        shoppingCart.add(product);
+    }
+
+    @Override
+    public Product find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Product> getBy(Supplier supplier) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getBy(ProductCategory productCategory) {
+        return null;
+    }
+}
