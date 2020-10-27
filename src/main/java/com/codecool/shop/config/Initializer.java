be6123d779667b02b36110c7ae642ae5b9ae0215
Mucenicalexandru <mucenic.alexandru@gmail.com>
2020-10-27 08:07:00 +0200

    inserted new suppliers, categories and products

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -26,16 +26,35 @@ public class Initializer implements ServletContextListener {
         //setting up a new supplier
         Supplier amazon = new Supplier("Amazon", "Digital content and services");
         supplierDataStore.add(amazon);
+
         Supplier lenovo = new Supplier("Lenovo", "Computers");
         supplierDataStore.add(lenovo);
 
+        Supplier playstation = new Supplier("Playstation", "Gaming");
+        supplierDataStore.add(playstation);
+
+        Supplier apple = new Supplier("IPhone", "Telecommunications");
+        supplierDataStore.add(apple);
+
+
         //setting up a new product category
         ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
+        ProductCategory console = new ProductCategory("Console", "Games", "Playstation, Xbox" );
+        ProductCategory phone = new ProductCategory("Phone", "Telecommunication", "Meet the new Iphone 12");
         productCategoryDataStore.add(tablet);
+        productCategoryDataStore.add(console);
+        productCategoryDataStore.add(phone);
+
+
 
         //setting up products and printing it
         productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
+
+        productDataStore.add(new Product("Iphone 12", 899, "USD", "5G speed. A14 Bionic, the fastest chip in a smartphone.\n" +
+                "An edge-to-edge OLED display. Ceramic Shield with four times better drop performance. And Night mode on every camera.", phone, apple));
+        productDataStore.add(new Product("Iphone 11", 699, "USD", "As part of our efforts to reach our environmental goals, iPhone 11 does not include a power adapter or EarPods. Included in the box is a USB‑C to Lightning cable that supports fast charging and is compatible with USB‑C power adapters and computer ports.", phone, apple));
+
     }
 }
