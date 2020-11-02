d6e666808511d00212ca5838abe2349ec10fc91a
Mucenicalexandru <mucenic.alexandru@gmail.com>
2020-11-02 13:30:53 +0200

    Products quantities updated with POST method

--- src/main/java/com/codecool/shop/controller/CartController.java
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -2,7 +2,10 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.CartDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.dao.implementation.ShoppingCartDaoMem;
+import com.codecool.shop.model.Product;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -12,7 +15,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 @WebServlet(urlPatterns = {"/cart"})
@@ -42,6 +47,35 @@ public class CartController extends HttpServlet {
         engine.process("cart/cart.html", context, resp.getWriter());
     }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String buttonPressed = req.getParameter("button");
+        int productId = Integer.parseInt(req.getParameter("productId"));
 
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        CartDao cartDataStore = ShoppingCartDaoMem.getInstance();
+        HashMap<Integer, Integer> quantity = cartDataStore.getQuantity();
+        List<Product> shoppingCartWithDuplicates = cartDataStore.getAll();
+        int quantityNumber = quantity.get(productId);
 
+        if(buttonPressed.equals("+")){
+            if (!shoppingCartWithDuplicates.contains(productDataStore.find(productId))) {
+                cartDataStore.add(productDataStore.find(productId));
+                quantity.put(productId, 1);
+            } else {
+                int a = quantity.get(productId) + 1;
+                quantity.replace(productId, a);
+            }
+        }else if(buttonPressed.equals("-")){
+            if(quantityNumber==1){
+                cartDataStore.remove(productDataStore.find(productId));
+                quantity.remove(productId);
+            }else{
+                quantityNumber--;
+                quantity.replace(productId,quantityNumber);
+            }
+        }
+
+        resp.sendRedirect("/cart");
+    }
 }
